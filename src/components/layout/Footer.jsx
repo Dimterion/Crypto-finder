e8f0fa590d4ba8c2c32dcb03165b4fbcf610f42a
Dimterion <dimterion59@gmail.com>
2022-11-18 11:58:46 +0100

    Added pages content and links.

---
+++ src/components/layout/Footer.jsx
@@ -0,0 +1,13 @@
+function Footer() {
+  const footerYear = new Date().getFullYear();
+
+  return (
+    <footer className="footer p-5 bg-gray-700 text-primary-content footer-center">
+      <div>
+        <p>Copyright &copy; {footerYear} All rights reserved</p>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
---
+++ src/pages/About.jsx
@@ -0,0 +1,15 @@
+function About() {
+  return (
+    <>
+      <h1 className="text-6xl mb-4">Finder App</h1>
+      <p className="mb-4 text-2xl font-light">
+        A React app to search GitHub profiles and see their details.
+        <p className="text-lg text-gray-400">
+          Version <span className="text-white">1.0.0</span>
+        </p>
+      </p>
+    </>
+  );
+}
+
+export default About;
