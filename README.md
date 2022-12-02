97254563a0c7d67d7e5d258d8fb3f4102247d55f
Dimterion <dimterion59@gmail.com>
2022-12-02 15:06:13 +0100

    Edited Readme, added header above the search.

--- README.md
+++ README.md
@@ -1,5 +1,15 @@
 # Crypto currencies finder application
 
-This is an application to search for and get information about the crupto currencies
+This is an application to search for and get information about crypto currencies
 
-WORK IN PROGRESS
+## Live version
+
+[Crypto finder](https://finder-app-tau.vercel.app/)
+
+## Description
+
+This application was built after remaking one of the previous projects from the React course on Udemy. Initial version is available [here](https://github.com/Dimterion/Github-finder) and its repository is [here](https://github.com/Dimterion/Github-finder).
+
+## Built with
+
+React, React Router, JavaScript, HTML, CSS, Tailwind CSS
--- src/components/currencies/CurrencySearch.jsx
+++ src/components/currencies/CurrencySearch.jsx
@@ -25,39 +25,44 @@ function CurrencySearch() {
   };
 
   return (
-    <div className="grid grid-cols-1 xl:grid-cols-2 lg:grid-cols-2 md:grid-cols-2 mb-8 gap-8">
-      <div>
-        <form onSubmit={handleSubmit}>
-          <div className="form-control">
-            <div className="relative">
-              <input
-                type="text"
-                className="w-full pr-40 bg-gray-200 input input-lg text-black rounded-none"
-                placeholder="Search"
-                value={text}
-                onChange={handleChange}
-              />
-              <button
-                type="submit"
-                className="absolute top-0 right-0  rounded-none w-30 btn btn-lg"
-              >
-                Go
-              </button>
-            </div>
-          </div>
-        </form>
-      </div>
-      {currencies.length > 0 && (
+    <>
+      <h2 className="text-lg font-bold mb-4">
+        Get information about crypto currencies
+      </h2>
+      <div className="grid grid-cols-1 xl:grid-cols-2 lg:grid-cols-2 md:grid-cols-2 mb-8 gap-8">
         <div>
-          <button
-            onClick={() => dispatch({ type: "CLEAR_CURRENCIES" })}
-            className="btn btn-ghost btn-lg"
-          >
-            Clear results
-          </button>
+          <form onSubmit={handleSubmit}>
+            <div className="form-control">
+              <div className="relative">
+                <input
+                  type="text"
+                  className="w-full pr-40 bg-gray-200 input input-lg text-black rounded-none"
+                  placeholder="Search"
+                  value={text}
+                  onChange={handleChange}
+                />
+                <button
+                  type="submit"
+                  className="absolute top-0 right-0  rounded-none w-30 btn btn-lg"
+                >
+                  Go
+                </button>
+              </div>
+            </div>
+          </form>
         </div>
-      )}
-    </div>
+        {currencies.length > 0 && (
+          <div>
+            <button
+              onClick={() => dispatch({ type: "CLEAR_CURRENCIES" })}
+              className="btn btn-ghost btn-lg"
+            >
+              Clear results
+            </button>
+          </div>
+        )}
+      </div>
+    </>
   );
 }
 
